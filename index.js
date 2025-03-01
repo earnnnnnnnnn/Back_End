035ae5805e461dfb872630a19be0a48e7f9f6533
eannnnnnnnn <s6606021631017@email.kmutnb.ac.th>
2025-03-02 03:44:25 +0700

    commit

--- index.js
+++ index.js
@@ -16,7 +16,138 @@ const sequelize = new Sequelize('database', 'username', 'password', {
     storage: './Database/CameraDB.sqlite',
 });
 
+//users
+const Users = sequelize.define('users', {
+    users_id: {
+        type: Sequelize.STRING,
+        // autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    phone_number: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+//camera
+const Camera = sequelize.define('camera', {
+    camera_id: {
+        type: Sequelize.STRING,
+        // autoIncrement: true,
+        primaryKey: true
+    },
+    cameraname: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    brand: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    rental_price_per_day: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    replacement_cost: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    }
+});
+//rental
+
 // define the book model
+const Rental = sequelize.define('Rental', {
+    rental_id: {
+        type: Sequelize.STRING,
+        // autoIncrement: true,
+        primaryKey: true
+    },
+    start_data: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    end_data: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    total_price: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    users_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    camera_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+//return
+const Return = sequelize.define('return', {
+    returnl_id: {
+        type: Sequelize.STRING,
+        // autoIncrement: true,
+        primaryKey: true
+    },
+    rental_id: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    condition: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    extra_charge: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    }
+});
+
+//payment
+const Payment = sequelize.define('payment', {
+    payment_id: {
+        type: Sequelize.STRING,
+        // autoIncrement: true,
+        primaryKey: true
+    },
+    amount: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    payment_method: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    rental_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+
+// reservation
 const Reservation = sequelize.define('reservation', {
     reservation_id: {
         type: Sequelize.STRING,
@@ -45,7 +176,329 @@ const Reservation = sequelize.define('reservation', {
 // create the table if it doesn't exist
 sequelize.sync()
 
+//user
+// route to get all books
+app.get("/users", (req, res) => {
+    Users.findAll().then(users => {
+        res.json(users);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+ });
+ 
+ // route to get a book by id
+ app.get('/users/:id', (req, res) => {
+     Users.findByPk(req.params.id).then(users => {
+         if (!users)
+             res.status(404).send();
+         else
+             res.json(users);
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to add a new book
+ app.post('/users', (req, res) => {
+     Users.create(req.body).then(users => {
+         res.json(users);
+     }
+     ).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to update a book
+ app.put('/users/:id', (req, res) => {
+     Users.findByPk(req.params.id).then(users => {
+         if (!users)
+             res.status(404).send();
+         else
+             users.update(req.body).then(users => {
+                 res.json(users);
+             }).catch(err => {
+                 res.status(500).send(err);
+             });
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to delete a book
+ app.delete('/users/:id', (req, res) => {
+     Users.findByPk(req.params.id).then(users => {
+         if (!users)
+             res.status(404).send();
+         else
+             users.destroy().then(() => {
+                 res.json(users);
+             }).catch(err => {
+                 res.status(500).send(err);
+             });
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
 
+
+
+//camera
+app.get("/camera", (req, res) => {
+    Camera.findAll().then(camera => {
+        res.json(camera);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+ });
+ 
+ // route to get a book by id
+ app.get('/camara/:id', (req, res) => {
+     Camera.findByPk(req.params.id).then(camera => {
+         if (!camera)
+             res.status(404).send();
+         else
+             res.json(camera);
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to add a new book
+ app.post('/camera', (req, res) => {
+     Camera.create(req.body).then(camera => {
+         res.json(camera);
+     }
+     ).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to update a book
+ app.put('/camera/:id', (req, res) => {
+     Camera.findByPk(req.params.id).then(camera => {
+         if (!camera)
+             res.status(404).send();
+         else
+             camera.update(req.body).then(camera => {
+                 res.json(camera);
+             }).catch(err => {
+                 res.status(500).send(err);
+             });
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to delete a book
+ app.delete('/users/:id', (req, res) => {
+     Camera.findByPk(req.params.id).then(camera => {
+         if (!camera)
+             res.status(404).send();
+         else
+             camera.destroy().then(() => {
+                 res.json(camera);
+             }).catch(err => {
+                 res.status(500).send(err);
+             });
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+
+//rental
+app.get("/rental", (req, res) => {
+    Rental.findAll().then(rental => {
+        res.json(rental);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+ });
+ 
+ // route to get a book by id
+ app.get('/rental/:id', (req, res) => {
+     Rental.findByPk(req.params.id).then(rental => {
+         if (!rental)
+             res.status(404).send();
+         else
+             res.json(rental);
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to add a new book
+ app.post('/rental', (req, res) => {
+     Rental.create(req.body).then(rental => {
+         res.json(rental);
+     }
+     ).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to update a book
+ app.put('/rental/:id', (req, res) => {
+     Rental.findByPk(req.params.id).then(rental => {
+         if (!rental)
+             res.status(404).send();
+         else
+             rental.update(req.body).then(rental => {
+                 res.json(rental);
+             }).catch(err => {
+                 res.status(500).send(err);
+             });
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to delete a book
+ app.delete('/rental/:id', (req, res) => {
+     Rental.findByPk(req.params.id).then(rental => {
+         if (!rental)
+             res.status(404).send();
+         else
+             rental.destroy().then(() => {
+                 res.json(rental);
+             }).catch(err => {
+                 res.status(500).send(err);
+             });
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+
+
+//return
+app.get("/return", (req, res) => {
+    Return.findAll().then(returns => {
+        res.json(returns);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+ });
+ 
+ // route to get a book by id
+ app.get('/return/:id', (req, res) => {
+     Return.findByPk(req.params.id).then(returns => {
+         if (!returns)
+             res.status(404).send();
+         else
+             res.json(returns);
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to add a new book
+ app.post('/return', (req, res) => {
+     Return.create(req.body).then(returns => {
+         res.json(returns);
+     }
+     ).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to update a book
+ app.put('/return:id', (req, res) => {
+     Return.findByPk(req.params.id).then(returns => {
+         if (!returns)
+             res.status(404).send();
+         else
+             returns.update(req.body).then(returns => {
+                 res.json(returns);
+             }).catch(err => {
+                 res.status(500).send(err);
+             });
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to delete a book
+ app.delete('/return/:id', (req, res) => {
+     Return.findByPk(req.params.id).then(returns => {
+         if (!returns)
+             res.status(404).send();
+         else
+             returns.destroy().then(() => {
+                 res.json(returns);
+             }).catch(err => {
+                 res.status(500).send(err);
+             });
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+
+
+//payment
+app.get("/payment", (req, res) => {
+    Payment.findAll().then(payment => {
+        res.json(payment);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+ });
+ 
+ // route to get a book by id
+ app.get('/payment/:id', (req, res) => {
+     Payment.findByPk(req.params.id).then(payment => {
+         if (!payment)
+             res.status(404).send();
+         else
+             res.json(payment);
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to add a new book
+ app.post('/payment', (req, res) => {
+     Payment.create(req.body).then(payment => {
+         res.json(payment);
+     }
+     ).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to update a book
+ app.put('/payment:id', (req, res) => {
+     Payment.findByPk(req.params.id).then(payment => {
+         if (!payment)
+             res.status(404).send();
+         else
+             payment.update(req.body).then(payment => {
+                 res.json(payment);
+             }).catch(err => {
+                 res.status(500).send(err);
+             });
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+ 
+ // route to delete a book
+ app.delete('/return/:id', (req, res) => {
+     Payment.findByPk(req.params.id).then(payment => {
+         if (!payment)
+             res.status(404).send();
+         else
+             payment.destroy().then(() => {
+                 res.json(payment);
+             }).catch(err => {
+                 res.status(500).send(err);
+             });
+     }).catch(err => {
+         res.status(500).send(err);
+     });
+ });
+
+
+//reservation
 // route to get all books
 app.get("/reservation", (req, res) => {
     Reservation.findAll().then(reservation => {
